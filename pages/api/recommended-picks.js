c19b9ec120799ee3109d1d15860ddb3ba7b54927
FScaccheri <francocass2009@hotmail.com>
2021-05-09 00:08:56 -0300

    WIP: Web scrapping

--- pages/api/recommended-picks.js
+++ pages/api/recommended-picks.js
@@ -1,6 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import cheerio from "cheerio"
 
-export default (req, res) => {
+export default async (req, res) => {
+  const response = await fetch("https://www.dotabuff.com/heroes/anti-mage");
+  const html = await response.text();
+
+  const $ = cheerio.load(html);
+  const worstVersus = $("header:contains(Worst Versus)").parent().find('tbody').children();
+  const heroes = [];
+  for (const heroSelector of worstVersus) {
+    console.log(heroSelector.children[1])
+  }
   const recommendedPicks = ["morphling"];
   res.status(200).json(recommendedPicks);
 }
