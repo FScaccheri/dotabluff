7424e4db8b10a2716f2d6eb2d299f73c8dedb0d0
Hipolito Gomez Cuello <hipolito@innovid.com>
2021-05-09 01:02:04 -0300

    formatted names

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,11 @@ import Head from 'next/head'
 import { useState } from 'react'
 
 const styles = {
-  yourTeamHeroName: {
+  enemyTeamHeroName: {
+    color: "red",
+    fontSize: "80%",
+  },
+  recommendedPickHeroName: {
     color: "blue",
     fontSize: "80%",
   },
@@ -16,20 +20,35 @@ const styles = {
 
 export default function Home() {
   const [mainInput, setMainInput] = useState("");
-  const [yourTeam, setYourTeam] = useState([]);
-  const [recommendedPicks, setRecommendedPicks] = useState([]);
+  const [enemyTeam, setEnemyTeam] = useState([]);
+  const [recommendedPicks, setRecommendedPicks] = useState({});
 
   const addHero = async () => {
     const response = await fetch(`/api/recommended-picks?hero=${mainInput}`);
-    const newRecommendedPicks = await response.json();
-    setRecommendedPicks([...recommendedPicks, ...newRecommendedPicks]);
-    setYourTeam([...yourTeam, mainInput]);
+    const apiPicks = await response.json();
+    const newRecommendedPicks = Object.assign({}, recommendedPicks);
+    console.log(newRecommendedPicks);
+    for (const hero of apiPicks) {
+      if (newRecommendedPicks[hero.name]) {
+        newRecommendedPicks[hero.name].priority++;
+      } else {
+        newRecommendedPicks[hero.name] =  { name: hero.name, priority: 1 };
+      }
+    }
+    setRecommendedPicks(newRecommendedPicks);
+    setEnemyTeam([...enemyTeam, mainInput]);
     setMainInput("");
   }
 
   const devTools = async () => {
-    console.log(yourTeam);
+    console.log(enemyTeam);
     console.log(recommendedPicks);
+  }
+
+  const getFormattedRecommendedPicks = () => {
+    const picksList = Object.values(recommendedPicks);
+    const sortedList = picksList.sort((heroA, heroB) => heroB.priority - heroA.priority)
+    return sortedList.filter(hero => !enemyTeam.includes(hero.name.toLowerCase())).slice(0, 10).map(hero => <p style={styles.recommendedPickHeroName}>{hero.name}</p>);
   }
 
   return (
@@ -41,23 +60,18 @@ export default function Home() {
         <div>
           <input type="text" value={mainInput} onChange={(event) => setMainInput(event.target.value)}></input>
           <button onClick={addHero}>Add Hero</button>
-          <button onClick={devTools}>DEV TOOLS</button>
         </div>
         <div>
-          <p>Your team:</p>
-          {yourTeam.map((hero) => {
+          <p>Enemy team:</p>
+          {enemyTeam.map((hero) => {
             return (
-              <p style={styles.yourTeamHeroName}>{hero}</p>
+              <p style={styles.enemyTeamHeroName}>{hero}</p>
             );
           })}
         </div>
         <div>
           <p>Recommended picks:</p>
-          {recommendedPicks.map((hero) => {
-            return (
-              <p style={styles.yourTeamHeroName}>{hero.name}</p>
-            );
-          })}
+          {getFormattedRecommendedPicks()}
         </div>
       </main>
     </div>
